b8750e7dafb45bf45da486d0d4dd209f9483f46f
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2025-11-23 17:50:49 -0300

    adicionando novas bibliotecas

--- build.zig
+++ build.zig
@@ -14,6 +14,10 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.linkSystemLibrary("SDL2");
+    exe.linkSystemLibrary("SDL2_image");
+    exe.linkSystemLibrary("SDL2_mixer");
+    exe.linkSystemLibrary("SDL2_ttf");
+
     exe.linkLibC();
     b.installArtifact(exe);
 
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 const NatucciAA = @import("NatucciAA");
+
 const sdl = @cImport({
     @cInclude("SDL2/SDL.h");
+    @cInclude("SDL2/SDL_image.h");
+    @cInclude("SDL2/SDL_mixer.h");
+    @cInclude("SDL2/SDL_ttf.h");
 });
 
 var renderer: ?*sdl.SDL_Renderer = null;
@@ -14,39 +18,53 @@ pub fn main() !void {
     if (init() > 0) {
         return;
     }
-    defer {
-        sdl.SDL_Quit();
-        sdl.SDL_DestroyWindow(window);
-    }
+    defer quitEmAll();
 
     try loop();
 }
 
 pub fn init() u4 {
-    const initSdl: c_int = sdl.SDL_Init(sdl.SDL_INIT_EVERYTHING);
-
-    if (initSdl < 0) {
+    if (sdl.SDL_Init(sdl.SDL_INIT_EVERYTHING) < 0) {
         std.debug.print("Erro ao inicalizar SDL -> {s}\n", .{sdl.SDL_GetError()});
+        return 1;
+    }
 
+    if (sdl.Mix_OpenAudio(44100, sdl.MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+        std.debug.print("Erro ao inicalizar SDL_mixer -> {s}\n", .{sdl.Mix_GetError()});
+        return 1;
+    }
+
+    if (sdl.IMG_Init(sdl.IMG_INIT_JPG | sdl.IMG_INIT_PNG) < 0) {
+        std.debug.print("Erro ao inicalizar SDL_Img -> {s}\n", .{sdl.IMG_GetError()});
+        return 1;
+    }
+
+    if (sdl.TTF_Init() < 0) {
+        std.debug.print("Erro ao inicalizar SDL_ttf -> {s}\n", .{sdl.TTF_GetError()});
         return 1;
     }
 
     window = sdl.SDL_CreateWindow("NatucciAA", sdl.SDL_WINDOWPOS_UNDEFINED, sdl.SDL_WINDOWPOS_UNDEFINED, 1280, HEIGHT, sdl.SDL_WINDOW_SHOWN);
     if (window == null) {
         std.debug.print("Erro ao criar Janela -> {s}", .{sdl.SDL_GetError()});
-
         return 1;
     }
 
     renderer = sdl.SDL_CreateRenderer(window, -1, sdl.SDL_RENDERER_ACCELERATED);
     if (renderer == null) {
         std.debug.print("Erro ao criar renderer -> {s}", .{sdl.SDL_GetError()});
-
         return 1;
     }
 
     std.debug.print("FOI\n", .{});
     return 0;
+}
+
+pub fn quitEmAll() void {
+    sdl.Mix_Quit();
+    sdl.SDL_Quit();
+    sdl.SDL_DestroyWindow(window);
+    sdl.TTF_Quit();
 }
 
 pub fn loop() !void {
@@ -60,9 +78,14 @@ pub fn loop() !void {
                 else => {},
             }
         }
-
         _ = sdl.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         _ = sdl.SDL_RenderClear(renderer);
+
+        var rect: sdl.SDL_Rect = .{ .x = 20, .y = 20, .w = 100, .h = 100 };
+
+        _ = sdl.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        _ = sdl.SDL_RenderDrawRect(renderer, &rect);
+
         _ = sdl.SDL_RenderPresent(renderer);
     }
 }
