c474bb9893b9d0c2f9046b37b44e9a6e28f09be7
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2026-03-17 20:48:30 -0300

    conseguindo buildar agora

--- build.zig
+++ build.zig
@@ -21,25 +21,39 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("usb-1.0");
     exe.linkLibC();
 
-    // AASDK Integration
-    exe.addCSourceFile(.{
-        .file = b.path("src/core/aasdk/aasdk_wrapper.cpp"),
-        .flags = &[_][]const u8{"-std=c++17", "-stdlib=libstdc++"},
-    });
+    // AASDK Integration - Compilando com g++ para evitar conflito de ABI do libc++ do Zig
+    const cpp_sources = [_][]const u8{
+        "src/core/aasdk/aasdk_wrapper.cpp",
+        "src/core/aasdk/context/bluetooth/bluetooth_context.cpp",
+        "src/core/aasdk/context/usb/usb_context.cpp",
+    };
 
-    exe.addCSourceFile(.{
-        .file = b.path("src/core/aasdk/context/bluetooth/bluetooth_context.cpp"),
-        .flags = &[_][]const u8{"-std=c++17", "-stdlib=libstdc++"},
-    });
-
-    exe.addCSourceFile(.{
-        .file = b.path("src/core/aasdk/context/usb/usb_context.cpp"),
-        .flags = &[_][]const u8{"-std=c++17", "-stdlib=libstdc++"},
-    });
+    for (cpp_sources) |src| {
+        const gpp = b.addSystemCommand(&[_][]const u8{ "g++", "-std=c++17", "-fPIC" });
+        gpp.addArg("-c");
+        gpp.addFileArg(b.path(src));
+        gpp.addArgs(&[_][]const u8{
+            "-I/usr/local/include",
+            "-I/usr/include/dbus-1.0",
+            "-I/usr/lib/dbus-1.0/include",
+            "-I/usr/include/libusb-1.0",
+            "-I/usr/include/SDL2",
+            "-Isrc/core/aasdk",
+            "-Isrc/core/aasdk/context/usb",
+            "-Isrc/core/aasdk/context/bluetooth",
+        });
+        gpp.addArg("-o");
+        const obj = gpp.addOutputFileArg(b.fmt("{s}.o", .{std.fs.path.basename(src)}));
+        exe.addObjectFile(obj);
+    }
 
     exe.linkSystemLibrary("aasdk");
     exe.linkSystemLibrary("aap_protobuf");
-    exe.linkSystemLibrary("stdc++");
+    exe.addObjectFile(.{ .cwd_relative = "/usr/lib/libstdc++.so" });
+    exe.addObjectFile(.{ .cwd_relative = "/usr/lib/libgcc_s.so.1" });
+
+    // Ensure it can find the library at runtime
+    exe.addRPath(.{ .cwd_relative = "/usr/local/lib" });
 
     b.installArtifact(exe);
 
